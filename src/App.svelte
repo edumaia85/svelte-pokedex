e5fba75b886fcd0b55377fa8d1e025d073ef1f33
Eduardo Maia <edumaia909@gmail.com>
2024-08-15 14:26:41 -0300

    feat: define application routes

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,18 @@
 <script>
+    import Router from "svelte-spa-router";
+    import Home from "./routes/Home.svelte";
+    import NotFound from "./routes/NotFound.svelte";
+    import Pokemon from "./routes/Pokemon.svelte";
+
+    const routes = {
+        "/": Home,
+        "/pokemon/:name": Pokemon,
+        "*": NotFound,
+    };
 </script>
 
 <main>
-    <h1>Hello, World!</h1>
+    <Router {routes} />
 </main>
 
 <style>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,9 @@
+<script>
+    import DefaultLayout from "../layouts/DefaultLayout.svelte";
+</script>
+
+<main>
+    <DefaultLayout title="Pokedex - Página Inicial">
+        <h1>Teste</h1>
+    </DefaultLayout>
+</main>
