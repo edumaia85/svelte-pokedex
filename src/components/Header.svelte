4412936aa3e373f7f7236236add2a6edb42da830
Eduardo Maia <edumaia909@gmail.com>
2024-08-15 14:27:40 -0300

    feat: create default layout for the project and header

---
+++ src/components/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+    import logoImg from '../assets/pokeball-logo.png'
+</script>
+
+<header class="header">
+    <nav>
+        <a href="/">
+            <img src={logoImg} alt="" height="30" width="30">
+            Pokedex
+        </a>
+    </nav>
+</header>
+
+<style>
+    .header {
+        width: 100%;
+        height: 40px;
+        background: black;
+
+        display: flex;
+    }
+
+    .header nav {
+        width: 100%;
+        height: 100%;
+
+        display: flex;
+        align-items: center;
+    }
+
+    .header nav a {
+        color: #FFF;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+    }
+</style>
---
+++ src/layouts/DefaultLayout.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Header from "../components/Header.svelte";
+  export let title;
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
+
+<Header />
+
+<main class="container">
+  <slot />
+</main>
+
+<style>
+    .container {
+        flex: 1;
+    }
+</style>
