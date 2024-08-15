86b6e866d139a26d50c8d72cc936c7823ec23cf4
Eduardo Maia <edumaia909@gmail.com>
2024-08-15 15:39:47 -0300

    feat: fetch data from poke-api

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,9 +1,46 @@
 <script>
+    import { onMount } from "svelte";
+    import PokemonCard from "../components/PokemonCard.svelte";
     import DefaultLayout from "../layouts/DefaultLayout.svelte";
+
+    let pokemonList = [];
+    let loading = true;
+
+    onMount(async () => {
+        try {
+            const response = await fetch(
+                "https://pokeapi.co/api/v2/pokemon?limit=150",
+            );
+            const data = await response.json();
+
+            const detailedPokemons = await Promise.all(
+                data.results.map(async (pokemon) => {
+                    const res = await fetch(pokemon.url);
+                    return res.json();
+                }),
+            );
+
+            pokemonList = detailedPokemons;
+        } catch (err) {
+            console.log(err.message);
+        } finally {
+            loading = false;
+        }
+    });
 </script>
 
 <main>
     <DefaultLayout title="Pokedex - Página Inicial">
-        <h1>Teste</h1>
+        {#if loading}
+            <p>Carregando...</p>
+        {:else}
+            {#each pokemonList as pokemon}
+                <PokemonCard
+                    name={pokemon.name}
+                    image={pokemon.sprites.front_default}
+                    type={pokemon.types.map((typeInfo) => typeInfo.type.name)}
+                />
+            {/each}
+        {/if}
     </DefaultLayout>
 </main>
